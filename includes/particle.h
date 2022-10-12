d10132d052081f987c8a8146806c1eef9469dfc7
PrzemekAn <przemekandzejczak@gmail.com>
2022-10-12 12:54:08 +0200

    new functions added

--- includes/particle.h
+++ includes/particle.h
@@ -3,10 +3,14 @@
 #include <vector>
 #include <memory>
 
+class Positive;
+class Negative;
 
 class Particle{
     public:
-        virtual void change_attributes_after_hit(Particle&) = 0;
+        virtual void change_attributes_after_hit(Particle& particle) = 0;
+        virtual void change_attributes_after_hit(Positive& poisitive_particle);
+        virtual void change_attributes_after_hit(Negative& negative_particle);
         virtual const int get_id() = 0;
         virtual const float get_mass() = 0;
         virtual const std::vector<float> get_position() = 0;
--- src/particle.cpp
+++ src/particle.cpp
@@ -1,6 +1,14 @@
 #include "particle.h"
 #include <iostream>
 
+void Particle::change_attributes_after_hit(Positive & poisitive_particle){
+    change_attributes_after_hit(poisitive_particle);
+}
+
+void Particle::change_attributes_after_hit(Negative & negative_particle){
+    change_attributes_after_hit(negative_particle);
+}
+
 Particle::~Particle(){
     // std::cout << "Particle's pure virtual destructor is called\n";
 }
