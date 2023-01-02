dc26881a62c9d52f3bfd286d4ffe545c770d21a7
PrzemekAn <przemekandrzejczak@gmail.com>
2023-01-02 10:32:51 +0100

    testing environment added

--- src/particle_simulator.cpp
+++ src/particle_simulator.cpp
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
     // std::unique_ptr<Simulation_data> sim_data(new Simulation_data_generator(part_count_for_generator));
     // Physics_engine engine(std::move(sim_data));
     std::unique_ptr<Simulation_data> sim_data_file(new 
-    Simulation_data_from_file("../project_assumptions/Output_example.json"));
+    Simulation_data_from_file("./Output_example.json"));
     Physics_engine engine(std::move(sim_data_file));
 
     long unsigned int part_count {engine.get_simulation_data().particles().size()};
